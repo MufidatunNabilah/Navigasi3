a374b03012ff6e6f824276ea75eec11c75ffa8f5
Mufidatun Nabilah <mufida.nabilah3@gmail.com>
2017-06-08 16:35:05 -0700

    Finnal

---
+++ app/src/main/java/layout/Page2.java
@@ -0,0 +1,30 @@
+package layout;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import xir3n19.remidiuts.comtelkomschid.example.mufidatunnabilah.navigasi3.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Page2 extends Fragment {
+
+
+    public Page2() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_page2, container, false);
+    }
+
+}
